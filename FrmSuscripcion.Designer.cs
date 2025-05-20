0e689d852228b986c8ab422cdef72753d505347c
Ander45Portafol <anderdeco25@gmail.com>
2025-05-19 21:36:07 -0600

    Registrar Usuarios

--- FrmSuscripcion.Designer.cs
+++ FrmSuscripcion.Designer.cs
@@ -34,14 +34,16 @@
             this.pnlsuscrip = new System.Windows.Forms.Panel();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.label3 = new System.Windows.Forms.Label();
-            this.txtcorreo = new System.Windows.Forms.TextBox();
+            this.TxtNombre = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
-            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.DtNacimiento = new System.Windows.Forms.DateTimePicker();
+            this.TxtClave = new System.Windows.Forms.TextBox();
+            this.TxtConfirm = new System.Windows.Forms.TextBox();
             this.BtnCerrar = new System.Windows.Forms.Button();
+            this.TxtCorreo = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
             this.pnlsuscrip.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
@@ -53,12 +55,13 @@
             this.btnIniciar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnIniciar.ForeColor = System.Drawing.Color.White;
             this.btnIniciar.Location = new System.Drawing.Point(415, 489);
-            this.btnIniciar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnIniciar.Margin = new System.Windows.Forms.Padding(4);
             this.btnIniciar.Name = "btnIniciar";
             this.btnIniciar.Size = new System.Drawing.Size(240, 47);
             this.btnIniciar.TabIndex = 64;
             this.btnIniciar.Text = "Regístrate";
             this.btnIniciar.UseVisualStyleBackColor = false;
+            this.btnIniciar.Click += new System.EventHandler(this.btnIniciar_Click);
             // 
             // label1
             // 
@@ -76,7 +79,7 @@
             this.pnlsuscrip.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(61)))), ((int)(((byte)(62)))), ((int)(((byte)(155)))));
             this.pnlsuscrip.Controls.Add(this.pictureBox1);
             this.pnlsuscrip.Location = new System.Drawing.Point(0, 0);
-            this.pnlsuscrip.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pnlsuscrip.Margin = new System.Windows.Forms.Padding(4);
             this.pnlsuscrip.Name = "pnlsuscrip";
             this.pnlsuscrip.Size = new System.Drawing.Size(267, 555);
             this.pnlsuscrip.TabIndex = 65;
@@ -85,7 +88,7 @@
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
             this.pictureBox1.Location = new System.Drawing.Point(0, 171);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(267, 204);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -96,23 +99,23 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(308, 171);
+            this.label3.Location = new System.Drawing.Point(308, 234);
             this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(123, 23);
             this.label3.TabIndex = 68;
             this.label3.Text = "Contraseña";
             // 
-            // txtcorreo
+            // TxtNombre
             // 
-            this.txtcorreo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(217)))), ((int)(((byte)(217)))), ((int)(((byte)(217)))));
-            this.txtcorreo.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.txtcorreo.Location = new System.Drawing.Point(313, 123);
-            this.txtcorreo.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.txtcorreo.Multiline = true;
-            this.txtcorreo.Name = "txtcorreo";
-            this.txtcorreo.Size = new System.Drawing.Size(441, 25);
-            this.txtcorreo.TabIndex = 67;
+            this.TxtNombre.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(217)))), ((int)(((byte)(217)))), ((int)(((byte)(217)))));
+            this.TxtNombre.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.TxtNombre.Location = new System.Drawing.Point(313, 123);
+            this.TxtNombre.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.TxtNombre.Multiline = true;
+            this.TxtNombre.Name = "TxtNombre";
+            this.TxtNombre.Size = new System.Drawing.Size(441, 25);
+            this.TxtNombre.TabIndex = 67;
             // 
             // label2
             // 
@@ -121,61 +124,61 @@
             this.label2.Location = new System.Drawing.Point(308, 97);
             this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(195, 23);
+            this.label2.Size = new System.Drawing.Size(111, 29);
             this.label2.TabIndex = 66;
-            this.label2.Text = "Correo electrónico";
+            this.label2.Text = "Nombre";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(308, 242);
+            this.label4.Location = new System.Drawing.Point(308, 305);
             this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(197, 23);
+            this.label4.Size = new System.Drawing.Size(222, 23);
             this.label4.TabIndex = 70;
-            this.label4.Text = "Nombre de usuario";
+            this.label4.Text = "Confirmar contraseña";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(308, 316);
+            this.label5.Location = new System.Drawing.Point(309, 389);
             this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(223, 23);
             this.label5.TabIndex = 72;
             this.label5.Text = "Fecha de Nacimiento";
             // 
-            // dateTimePicker1
+            // DtNacimiento
             // 
-            this.dateTimePicker1.Location = new System.Drawing.Point(313, 343);
-            this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(440, 22);
-            this.dateTimePicker1.TabIndex = 73;
+            this.DtNacimiento.Location = new System.Drawing.Point(314, 416);
+            this.DtNacimiento.Margin = new System.Windows.Forms.Padding(4);
+            this.DtNacimiento.Name = "DtNacimiento";
+            this.DtNacimiento.Size = new System.Drawing.Size(440, 22);
+            this.DtNacimiento.TabIndex = 73;
             // 
-            // textBox2
+            // TxtClave
             // 
-            this.textBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(217)))), ((int)(((byte)(217)))), ((int)(((byte)(217)))));
-            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.textBox2.Location = new System.Drawing.Point(313, 197);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.textBox2.Multiline = true;
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(441, 25);
-            this.textBox2.TabIndex = 76;
+            this.TxtClave.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(217)))), ((int)(((byte)(217)))), ((int)(((byte)(217)))));
+            this.TxtClave.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.TxtClave.Location = new System.Drawing.Point(313, 260);
+            this.TxtClave.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.TxtClave.Multiline = true;
+            this.TxtClave.Name = "TxtClave";
+            this.TxtClave.Size = new System.Drawing.Size(441, 25);
+            this.TxtClave.TabIndex = 76;
             // 
-            // textBox1
+            // TxtConfirm
             // 
-            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(217)))), ((int)(((byte)(217)))), ((int)(((byte)(217)))));
-            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.textBox1.Location = new System.Drawing.Point(313, 268);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(441, 25);
-            this.textBox1.TabIndex = 77;
+            this.TxtConfirm.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(217)))), ((int)(((byte)(217)))), ((int)(((byte)(217)))));
+            this.TxtConfirm.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.TxtConfirm.Location = new System.Drawing.Point(313, 331);
+            this.TxtConfirm.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.TxtConfirm.Multiline = true;
+            this.TxtConfirm.Name = "TxtConfirm";
+            this.TxtConfirm.Size = new System.Drawing.Size(441, 25);
+            this.TxtConfirm.TabIndex = 77;
             // 
             // BtnCerrar
             // 
@@ -190,26 +193,50 @@
             this.BtnCerrar.UseVisualStyleBackColor = true;
             this.BtnCerrar.Click += new System.EventHandler(this.BtnCerrar_Click);
             // 
+            // TxtCorreo
+            // 
+            this.TxtCorreo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(217)))), ((int)(((byte)(217)))), ((int)(((byte)(217)))));
+            this.TxtCorreo.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.TxtCorreo.Location = new System.Drawing.Point(314, 194);
+            this.TxtCorreo.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.TxtCorreo.Multiline = true;
+            this.TxtCorreo.Name = "TxtCorreo";
+            this.TxtCorreo.Size = new System.Drawing.Size(441, 25);
+            this.TxtCorreo.TabIndex = 81;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(309, 168);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(195, 23);
+            this.label6.TabIndex = 80;
+            this.label6.Text = "Correo electrónico";
+            // 
             // FrmSuscripcion
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(847, 554);
+            this.Controls.Add(this.TxtCorreo);
+            this.Controls.Add(this.label6);
             this.Controls.Add(this.BtnCerrar);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.TxtConfirm);
+            this.Controls.Add(this.TxtClave);
+            this.Controls.Add(this.DtNacimiento);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.txtcorreo);
+            this.Controls.Add(this.TxtNombre);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.pnlsuscrip);
             this.Controls.Add(this.btnIniciar);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "FrmSuscripcion";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "FrmSuscripcion";
@@ -228,13 +255,15 @@
         private System.Windows.Forms.Panel pnlsuscrip;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox txtcorreo;
+        private System.Windows.Forms.TextBox TxtNombre;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.DateTimePicker dateTimePicker1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DateTimePicker DtNacimiento;
+        private System.Windows.Forms.TextBox TxtClave;
+        private System.Windows.Forms.TextBox TxtConfirm;
         private System.Windows.Forms.Button BtnCerrar;
+        private System.Windows.Forms.TextBox TxtCorreo;
+        private System.Windows.Forms.Label label6;
     }
 }
