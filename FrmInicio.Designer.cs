c2bf6f35f0972fb6791c43a80da583062366fe92
Ander45Portafol <anderdeco25@gmail.com>
2025-05-20 21:58:18 -0600

    Subiendo arreglo en el diseño del formulario de inicio

--- FrmInicio.Designer.cs
+++ FrmInicio.Designer.cs
@@ -29,237 +29,32 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmInicio));
-            this.label8 = new System.Windows.Forms.Label();
-            this.dgvnovedades = new System.Windows.Forms.DataGridView();
             this.label1 = new System.Windows.Forms.Label();
-            this.pnlmusica = new System.Windows.Forms.Panel();
-            this.pictureBox25 = new System.Windows.Forms.PictureBox();
-            this.pictureBox24 = new System.Windows.Forms.PictureBox();
-            this.pictureBox23 = new System.Windows.Forms.PictureBox();
-            this.pictureBox22 = new System.Windows.Forms.PictureBox();
-            this.label10 = new System.Windows.Forms.Label();
-            this.label9 = new System.Windows.Forms.Label();
-            this.pictureBox21 = new System.Windows.Forms.PictureBox();
-            this.pct2 = new System.Windows.Forms.PictureBox();
-            this.pct3 = new System.Windows.Forms.PictureBox();
-            this.pct4 = new System.Windows.Forms.PictureBox();
-            this.pct5 = new System.Windows.Forms.PictureBox();
-            this.pct6 = new System.Windows.Forms.PictureBox();
-            this.pct1 = new System.Windows.Forms.PictureBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            ((System.ComponentModel.ISupportInitialize)(this.dgvnovedades)).BeginInit();
-            this.pnlmusica.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox25)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox24)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox23)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox22)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox21)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pct2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pct3)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pct4)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pct5)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pct6)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pct1)).BeginInit();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // label8
-            // 
-            this.label8.AutoSize = true;
-            this.label8.Font = new System.Drawing.Font("Century Gothic", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.Location = new System.Drawing.Point(119, 400);
-            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(178, 34);
-            this.label8.TabIndex = 42;
-            this.label8.Text = "Listas para ti";
-            // 
-            // dgvnovedades
-            // 
-            this.dgvnovedades.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(217)))), ((int)(((byte)(217)))), ((int)(((byte)(217)))));
-            this.dgvnovedades.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.dgvnovedades.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvnovedades.Location = new System.Drawing.Point(124, 150);
-            this.dgvnovedades.Margin = new System.Windows.Forms.Padding(4);
-            this.dgvnovedades.Name = "dgvnovedades";
-            this.dgvnovedades.RowHeadersWidth = 51;
-            this.dgvnovedades.Size = new System.Drawing.Size(976, 216);
-            this.dgvnovedades.TabIndex = 41;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Century Gothic", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(119, 101);
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(286, 87);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(172, 34);
-            this.label1.TabIndex = 40;
-            this.label1.Text = "Novedades";
+            this.label1.Size = new System.Drawing.Size(556, 47);
+            this.label1.TabIndex = 42;
+            this.label1.Text = "¡BIENVENIDO A HARMONIQ!";
             // 
-            // pnlmusica
+            // pictureBox1
             // 
-            this.pnlmusica.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.pnlmusica.Controls.Add(this.pictureBox25);
-            this.pnlmusica.Controls.Add(this.pictureBox24);
-            this.pnlmusica.Controls.Add(this.pictureBox23);
-            this.pnlmusica.Controls.Add(this.pictureBox22);
-            this.pnlmusica.Controls.Add(this.label10);
-            this.pnlmusica.Controls.Add(this.label9);
-            this.pnlmusica.Controls.Add(this.pictureBox21);
-            this.pnlmusica.Location = new System.Drawing.Point(75, 596);
-            this.pnlmusica.Margin = new System.Windows.Forms.Padding(4);
-            this.pnlmusica.Name = "pnlmusica";
-            this.pnlmusica.Size = new System.Drawing.Size(1025, 135);
-            this.pnlmusica.TabIndex = 49;
-            // 
-            // pictureBox25
-            // 
-            this.pictureBox25.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox25.Image")));
-            this.pictureBox25.Location = new System.Drawing.Point(383, 73);
-            this.pictureBox25.Margin = new System.Windows.Forms.Padding(4);
-            this.pictureBox25.Name = "pictureBox25";
-            this.pictureBox25.Size = new System.Drawing.Size(379, 31);
-            this.pictureBox25.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox25.TabIndex = 40;
-            this.pictureBox25.TabStop = false;
-            // 
-            // pictureBox24
-            // 
-            this.pictureBox24.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox24.Image")));
-            this.pictureBox24.Location = new System.Drawing.Point(651, 23);
-            this.pictureBox24.Margin = new System.Windows.Forms.Padding(4);
-            this.pictureBox24.Name = "pictureBox24";
-            this.pictureBox24.Size = new System.Drawing.Size(24, 31);
-            this.pictureBox24.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox24.TabIndex = 39;
-            this.pictureBox24.TabStop = false;
-            // 
-            // pictureBox23
-            // 
-            this.pictureBox23.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox23.Image")));
-            this.pictureBox23.Location = new System.Drawing.Point(554, 23);
-            this.pictureBox23.Margin = new System.Windows.Forms.Padding(4);
-            this.pictureBox23.Name = "pictureBox23";
-            this.pictureBox23.Size = new System.Drawing.Size(24, 31);
-            this.pictureBox23.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox23.TabIndex = 38;
-            this.pictureBox23.TabStop = false;
-            // 
-            // pictureBox22
-            // 
-            this.pictureBox22.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox22.Image")));
-            this.pictureBox22.Location = new System.Drawing.Point(462, 23);
-            this.pictureBox22.Margin = new System.Windows.Forms.Padding(4);
-            this.pictureBox22.Name = "pictureBox22";
-            this.pictureBox22.Size = new System.Drawing.Size(28, 31);
-            this.pictureBox22.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox22.TabIndex = 37;
-            this.pictureBox22.TabStop = false;
-            // 
-            // label10
-            // 
-            this.label10.AutoSize = true;
-            this.label10.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label10.ForeColor = System.Drawing.Color.Black;
-            this.label10.Location = new System.Drawing.Point(120, 63);
-            this.label10.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(129, 19);
-            this.label10.TabIndex = 36;
-            this.label10.Text = "Artista - Album";
-            // 
-            // label9
-            // 
-            this.label9.AutoSize = true;
-            this.label9.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label9.ForeColor = System.Drawing.Color.Black;
-            this.label9.Location = new System.Drawing.Point(119, 37);
-            this.label9.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(176, 23);
-            this.label9.TabIndex = 35;
-            this.label9.Text = "Nombre canción";
-            // 
-            // pictureBox21
-            // 
-            this.pictureBox21.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox21.Image")));
-            this.pictureBox21.Location = new System.Drawing.Point(27, 26);
-            this.pictureBox21.Margin = new System.Windows.Forms.Padding(4);
-            this.pictureBox21.Name = "pictureBox21";
-            this.pictureBox21.Size = new System.Drawing.Size(89, 66);
-            this.pictureBox21.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox21.TabIndex = 0;
-            this.pictureBox21.TabStop = false;
-            // 
-            // pct2
-            // 
-            this.pct2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(217)))), ((int)(((byte)(217)))), ((int)(((byte)(217)))));
-            this.pct2.Location = new System.Drawing.Point(300, 456);
-            this.pct2.Margin = new System.Windows.Forms.Padding(4);
-            this.pct2.Name = "pct2";
-            this.pct2.Size = new System.Drawing.Size(111, 113);
-            this.pct2.TabIndex = 48;
-            this.pct2.TabStop = false;
-            // 
-            // pct3
-            // 
-            this.pct3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(217)))), ((int)(((byte)(217)))), ((int)(((byte)(217)))));
-            this.pct3.Location = new System.Drawing.Point(485, 456);
-            this.pct3.Margin = new System.Windows.Forms.Padding(4);
-            this.pct3.Name = "pct3";
-            this.pct3.Size = new System.Drawing.Size(111, 113);
-            this.pct3.TabIndex = 47;
-            this.pct3.TabStop = false;
-            // 
-            // pct4
-            // 
-            this.pct4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(217)))), ((int)(((byte)(217)))), ((int)(((byte)(217)))));
-            this.pct4.Location = new System.Drawing.Point(657, 456);
-            this.pct4.Margin = new System.Windows.Forms.Padding(4);
-            this.pct4.Name = "pct4";
-            this.pct4.Size = new System.Drawing.Size(111, 113);
-            this.pct4.TabIndex = 46;
-            this.pct4.TabStop = false;
-            // 
-            // pct5
-            // 
-            this.pct5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(217)))), ((int)(((byte)(217)))), ((int)(((byte)(217)))));
-            this.pct5.Location = new System.Drawing.Point(822, 456);
-            this.pct5.Margin = new System.Windows.Forms.Padding(4);
-            this.pct5.Name = "pct5";
-            this.pct5.Size = new System.Drawing.Size(111, 113);
-            this.pct5.TabIndex = 45;
-            this.pct5.TabStop = false;
-            // 
-            // pct6
-            // 
-            this.pct6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(217)))), ((int)(((byte)(217)))), ((int)(((byte)(217)))));
-            this.pct6.Location = new System.Drawing.Point(989, 456);
-            this.pct6.Margin = new System.Windows.Forms.Padding(4);
-            this.pct6.Name = "pct6";
-            this.pct6.Size = new System.Drawing.Size(111, 113);
-            this.pct6.TabIndex = 44;
-            this.pct6.TabStop = false;
-            // 
-            // pct1
-            // 
-            this.pct1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(217)))), ((int)(((byte)(217)))), ((int)(((byte)(217)))));
-            this.pct1.Location = new System.Drawing.Point(124, 456);
-            this.pct1.Margin = new System.Windows.Forms.Padding(4);
-            this.pct1.Name = "pct1";
-            this.pct1.Size = new System.Drawing.Size(111, 113);
-            this.pct1.TabIndex = 43;
-            this.pct1.TabStop = false;
-            // 
-            // textBox1
-            // 
-            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(217)))), ((int)(((byte)(217)))), ((int)(((byte)(217)))));
-            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.textBox1.Location = new System.Drawing.Point(199, 27);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(842, 49);
-            this.textBox1.TabIndex = 50;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(329, 157);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(448, 317);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 43;
+            this.pictureBox1.TabStop = false;
             // 
             // FrmInicio
             // 
@@ -267,16 +62,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(1174, 744);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.pnlmusica);
-            this.Controls.Add(this.pct2);
-            this.Controls.Add(this.pct3);
-            this.Controls.Add(this.pct4);
-            this.Controls.Add(this.pct5);
-            this.Controls.Add(this.pct6);
-            this.Controls.Add(this.pct1);
-            this.Controls.Add(this.label8);
-            this.Controls.Add(this.dgvnovedades);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Margin = new System.Windows.Forms.Padding(4);
@@ -284,43 +70,15 @@
             this.Text = "FrmInicio";
             this.Load += new System.EventHandler(this.FrmInicio_Load);
             this.Resize += new System.EventHandler(this.FrmInicio_Resize);
-            ((System.ComponentModel.ISupportInitialize)(this.dgvnovedades)).EndInit();
-            this.pnlmusica.ResumeLayout(false);
-            this.pnlmusica.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox25)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox24)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox23)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox22)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox21)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pct2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pct3)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pct4)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pct5)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pct6)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pct1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-        private System.Windows.Forms.PictureBox pct2;
-        private System.Windows.Forms.PictureBox pct3;
-        private System.Windows.Forms.PictureBox pct4;
-        private System.Windows.Forms.PictureBox pct5;
-        private System.Windows.Forms.PictureBox pct6;
-        private System.Windows.Forms.PictureBox pct1;
-        private System.Windows.Forms.Label label8;
-        private System.Windows.Forms.DataGridView dgvnovedades;
+
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Panel pnlmusica;
-        private System.Windows.Forms.PictureBox pictureBox25;
-        private System.Windows.Forms.PictureBox pictureBox24;
-        private System.Windows.Forms.PictureBox pictureBox23;
-        private System.Windows.Forms.PictureBox pictureBox22;
-        private System.Windows.Forms.Label label10;
-        private System.Windows.Forms.Label label9;
-        private System.Windows.Forms.PictureBox pictureBox21;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
